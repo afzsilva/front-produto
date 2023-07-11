bf8cb20f0a71d2cdfa5b1d89edc2f443f266c82a
afzsilva <afzsilva@gmail.com>
2023-07-11 16:06:54 -0300

    implementa metodos CRUD no component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,64 @@ export class AppComponent {
 
 
   constructor(private fb:FormBuilder,private service:ProdutoService){
-
+      this.buscarProdutos();
   }
+
+  buscarProdutos() {
+    this.service.buscarTodos().subscribe(
+     {
+       next: (res) => {
+        this.produtos = res;
+       },
+       error: (error) => {
+        console.error(error);
+       },
+       complete: () => console.log('lista de produtos', this.produtos)
+     }
+    )
+  }
+
+  criarProduto(): Produto {
+    return {
+      id: Number(this.produtoForm.get('id')?.value),
+      nome: String(this.produtoForm.get('nome')?.value),
+      descricao: String(this.produtoForm.get('descricao')?.value),
+      valor: Number(this.produtoForm.get('preco')?.value)
+    }
+  }
+
+
+  salvar() {
+    if (this.produtoForm.valid) {
+      const produto = this.criarProduto();
+      this.service.salvar(produto).subscribe(
+        {
+          next: (res) => {
+            this.produtoForm.reset();
+            this.buscarProdutos();
+            alert("Produto salvo com sucesso");
+          },
+          error: (error) => {
+            console.log(error);
+          },
+          complete: () => console.log('produto salvo')
+        }
+      )
+    }
+  }
+
+  remover(produto: Produto) {
+    const confirmacao = confirm("Quer realmente excluir esse produto?");
+    if (confirmacao) {
+      this.service.remover(produto.id).subscribe({
+        next: () => {
+          this.buscarProdutos();
+          alert("Produto removido com sucesso");
+        }
+      })
+    }
+  }
+
 }
+
+
