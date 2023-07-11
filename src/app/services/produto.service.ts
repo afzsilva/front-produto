f6430285549229d17451ec12be148ee9780c1173
afzsilva <afzsilva@gmail.com>
2023-07-11 14:22:32 -0300

    Implementa metodos CRUD servicos

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Produto } from '../model/produto';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,20 @@ export class ProdutoService {
 
   private _URL = environment.url + 'produtos';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  salvar(produto: Produto) {
+    return this.http.post<Produto>(this._URL, produto);
+  }
+
+  buscarTodos() {
+    return this.http.get<Produto[]>(this._URL);
+  }
+
+  remover(id: number) {
+    return this.http.delete(`${this._URL}/${id}`);
+  }
+
+
+
 }
